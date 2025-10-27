bfb0c2d9f540e31e11756b62b5dc70f3446f6e19
yasodakumari <136498356+yasodakumari@users.noreply.github.com>
2025-10-27 19:34:04 +0530

    Add files via upload

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from  '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext'
+
+const Navbar = () => {
+
+  const [menu,setMenu] = useState("shop");
+  const {getTotalCartItems}= useContext(ShopContext);
+
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt="" />
+            <p>Shopper</p>
+        </div>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("shop")}}><Link  style={{ textDecoration: 'none' }}  to='/'>Shop</Link>{menu==="shop"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link style={{ textDecoration: 'none' }}  to='/mens'>Men</Link>{menu==="mens"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("women's")}}><Link style={{ textDecoration: 'none' }}  to='/womens'>Women</Link>{menu==="women's"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none' }}  to='/kids'>kids</Link>{menu==="kids"?<h/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+
